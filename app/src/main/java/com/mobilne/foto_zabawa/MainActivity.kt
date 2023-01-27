b2e152c9fde902cc4baf86cc82dc96c942a28a94
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-01-27 22:19:03 +0100

    FT-5: Implement Dagger-Hilt for DI

--- app/src/main/java/com/mobilne/foto_zabawa/MainActivity.kt
+++ app/src/main/java/com/mobilne/foto_zabawa/MainActivity.kt
@@ -3,8 +3,8 @@ package com.mobilne.foto_zabawa
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowInsetsController
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,8 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mobilne.foto_zabawa.ui.theme.FotozabawaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
---
+++ app/src/main/java/com/mobilne/foto_zabawa/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.mobilne.foto_zabawa.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AppModule {
+
+}
