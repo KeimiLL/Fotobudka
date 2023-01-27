debdf8f0ce8715f7ac994a64402eaf73ac49cf76
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-01-27 20:14:43 +0100

    FT-3: Add fullscreen and set orientation to horizontal

--- app/src/main/java/com/mobilne/foto_zabawa/MainActivity.kt
+++ app/src/main/java/com/mobilne/foto_zabawa/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.mobilne.foto_zabawa
 
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -25,6 +27,30 @@ class MainActivity : ComponentActivity() {
                     Greeting("Android")
                 }
             }
+        }
+
+        setFullScreen()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        setFullScreen()
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        setFullScreen()
+    }
+
+    private fun setFullScreen() {
+        window.setDecorFitsSystemWindows(false)
+        val controller = window.insetsController
+        if (controller != null) {
+            controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            controller.systemBarsBehavior =
+                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
 }
