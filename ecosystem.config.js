9f9485d89f3e5f284b7f9b810b7aefdd44d48e0e
Damian Szuster <adverstm@gmail.com>
2023-01-31 06:23:05 +0100

    FT-10: Init project

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'fotobudka-api',
+      script: 'npm',
+      args: 'run production',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss.SSS',
+      watch: false,
+      ignore_watch: ['node_modules', 'files'],
+      vizion: true,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      }
+    }
+  ]
+}
