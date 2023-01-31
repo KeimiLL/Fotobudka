d2684cd380d0fa29f22f8b5da24bf9318fb321c8
Damian Szuster <adverstm@gmail.com>
2023-01-31 22:26:57 +0100

    FT-11: Settings and photo upload

--- services/routes.js
+++ services/routes.js
@@ -2,7 +2,8 @@ const auth = require('../middleware/auth'),
   multer = require('multer'),
   os = require('os'),
   path = require('path'),
-  upload = multer({ dest: path.join(os.tmpdir(), 'fotobudka') })
+  upload = multer({ dest: path.join(os.tmpdir(), 'fotobudka') }),
+  storage = require('../services/storage')
 
 module.exports = app => {
   app.use(auth)
@@ -11,21 +12,55 @@ module.exports = app => {
     res.sendStatus(404)
   })
 
-  app.get('/settings/:uid', async (req, res, next) => {
-
-  })
+  app.get('/settings/:uid', async (req, res, next) =>
+    storage.readSettings(req.params.uid).then(data => {
+      res.json(data)
+    }).catch(err => {
+      res.sendStatus(err.code === 'ENOENT' ? 404 : 500)
+      if (err.code === 'ENOENT') return
+      console.error('Error reading settings:', err)
+    }))
 
   app.post('/settings/:uid', (req, res, next) => {
     console.log(req.body)
 
+    storage.writeSettings(req.params.uid, req.body).then(() => {
+      res.sendStatus(200)
+    }).catch(err => {
+      res.sendStatus(500)
+      console.error('Error writing settings:', err)
+    })
   })
 
   app.post('/photo/:uid', upload.single('file'), (req, res, next) => {
-
+    storage.addPhoto(req.params.uid, req.file, req.body).then(() => {
+      res.sendStatus(200)
+    }).catch(err => {
+      res.sendStatus(500)
+      console.error('Error adding photo:', err)
+    })
   })
 
   app.get('/status/:uid', (req, res, next) => {
+    storage.readSettings(req.params.uid).then(settings => {
+      const data = { settings }
 
+      storage.listPhotos(req.params.uid).then(photos => {
+        data.photos = photos.filter(f => f.endsWith('.jpg'))
+        res.json(data)
+
+      }).catch(err => {
+        if (err.code === 'ENOENT')
+          return res.json(data)
+
+        res.sendStatus(500)
+        console.error('Error getting photos:', err)
+      })
+
+    }).catch(err => {
+      res.sendStatus(err.code === 'ENOENT' ? 404 : 500)
+      if (err.code === 'ENOENT') return
+      console.error('Error getting status:', err)
+    })
   })
 }
-
