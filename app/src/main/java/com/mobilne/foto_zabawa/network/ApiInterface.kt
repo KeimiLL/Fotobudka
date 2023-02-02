3f31e33a6f0142657ffe2a429fb84b33695e63ed
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-02-02 22:36:56 +0100

    FT-13: Add a method for test POST request

--- app/src/main/java/com/mobilne/foto_zabawa/network/ApiInterface.kt
+++ app/src/main/java/com/mobilne/foto_zabawa/network/ApiInterface.kt
@@ -1,7 +1,13 @@
 package com.mobilne.foto_zabawa.network
 
 import com.mobilne.foto_zabawa.model.TestResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
 import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 import javax.inject.Singleton
 
 @Singleton
@@ -9,4 +15,12 @@ interface ApiInterface {
 
     @GET("status/test")
     suspend fun getStatusTest(): TestResponse
+
+    @Multipart
+    @POST("photo/test")
+    suspend fun postPhotoTest(
+        @Part imagePart: MultipartBody.Part,
+        @Part("cardId") cardId: RequestBody,
+        @Part("cardText") cardText: RequestBody,
+    ): ResponseBody
 }
