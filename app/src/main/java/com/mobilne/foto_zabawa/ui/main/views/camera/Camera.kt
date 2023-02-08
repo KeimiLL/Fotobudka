e3b6dce0872f5a0b2d1e0085318272cb491b0e44
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-02-08 15:14:44 +0100

    FT-20: PhotoCard and Camera update

--- app/src/main/java/com/mobilne/foto_zabawa/ui/main/views/camera/Camera.kt
+++ app/src/main/java/com/mobilne/foto_zabawa/ui/main/views/camera/Camera.kt
@@ -3,21 +3,20 @@ package com.mobilne.foto_zabawa.ui.main.views.camera
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.net.toUri
 import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberImagePainter
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.mobilne.foto_zabawa.R
 import com.mobilne.foto_zabawa.ui.main.MainViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -53,20 +52,17 @@ fun MainContent(modifier: Modifier = Modifier, mainViewModel: MainViewModel) {
     val emptyImageUri = Uri.parse("file://dev/null")
     var imageUri by remember { mutableStateOf(emptyImageUri) }
     if (imageUri != emptyImageUri) {
-        Box(modifier = modifier) {
+        Row(modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center) {
+            Text(text = "Your photos are ready to print!",
+            fontSize = 30.sp)
+            Spacer(Modifier.width(20.dp))
             Image(
-                modifier = Modifier.fillMaxSize(),
-                painter = rememberImagePainter(imageUri),
-                contentDescription = "Captured image"
+                painter = painterResource(id = R.drawable.photos),
+                contentDescription = "images",
+                modifier = Modifier.size(80.dp)
             )
-            Button(
-                modifier = Modifier.align(Alignment.BottomCenter),
-                onClick = {
-                    imageUri = emptyImageUri
-                }
-            ) {
-                Text("Remove image")
-            }
         }
     } else {
         CameraCapture(
