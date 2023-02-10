f9f10cb302c3c43a8a75fd8354cee511da40e906
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-02-10 22:50:09 +0100

    FT-23: Add displaying apiResponseCount

--- app/src/main/java/com/mobilne/foto_zabawa/ui/main/views/camera/CameraCapture.kt
+++ app/src/main/java/com/mobilne/foto_zabawa/ui/main/views/camera/CameraCapture.kt
@@ -9,16 +9,22 @@ import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.core.UseCase
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.mobilne.foto_zabawa.R
 import com.mobilne.foto_zabawa.ui.main.MainViewModel
@@ -50,28 +56,22 @@ fun CameraCapture(
         stringForNoPermissionLanguage = R.string.niepozwolenie
         stringForOpenSettings = R.string.ustawienia
     }
-    Permission(
-        permission = Manifest.permission.CAMERA,
+    Permission(permission = Manifest.permission.CAMERA,
         rationale = stringResource(stringForPermissionLanguage),
         mainViewModel = mainViewModel,
         permissionNotAvailableContent = {
             Column(modifier) {
                 Text(stringResource(stringForNoPermissionLanguage))
                 Spacer(modifier = Modifier.height(8.dp))
-                Button(
-                    onClick = {
-                        context.startActivity(
-                            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                                data = Uri.fromParts("package", context.packageName, null)
-                            }
-                        )
-                    }
-                ) {
+                Button(onClick = {
+                    context.startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                        data = Uri.fromParts("package", context.packageName, null)
+                    })
+                }) {
                     Text(stringResource(stringForOpenSettings))
                 }
             }
-        }
-    ) {
+        }) {
         Box(modifier = modifier) {
             val lifecycleOwner = LocalLifecycleOwner.current
             val coroutineScope = rememberCoroutineScope()
@@ -79,17 +79,13 @@ fun CameraCapture(
             val imageCaptureUseCase by remember {
                 mutableStateOf(
                     ImageCapture.Builder()
-                        .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
-                        .build()
+                        .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY).build()
                 )
             }
             Box {
-                CameraPreview(
-                    modifier = Modifier.fillMaxSize(),
-                    onUseCase = {
-                        previewUseCase = it
-                    }
-                )
+                CameraPreview(modifier = Modifier.fillMaxSize(), onUseCase = {
+                    previewUseCase = it
+                })
                 CapturePictureButton(
                     modifier = Modifier
                         .size(100.dp)
@@ -107,7 +103,7 @@ fun CameraCapture(
 //                            }
 //                        }, ((mainViewModel.getValue(0)) * 1000).toLong())
 //                        Interval
-                        var counter: Int = 0
+                        var counter = 0
                         fixedRateTimer(
                             name = "alert-timer",
                             initialDelay = 0,
@@ -147,6 +143,20 @@ fun CameraCapture(
                     },
                     mainViewModel = mainViewModel
                 )
+                val background = if (mainViewModel.isButtonEnable && mainViewModel.apiResponseCount == 0) Color.Transparent else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(bottomStartPercent = 20))
+                        .align(Alignment.TopEnd)
+                        .background(background)
+                        .padding(8.dp)
+                ) {
+                    Text(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        text = mainViewModel.getApiResponseCountDisplayText()
+                    )
+                }
             }
             LaunchedEffect(previewUseCase) {
                 val cameraProvider = context.getCameraProvider()
